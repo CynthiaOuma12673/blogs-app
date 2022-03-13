d39468af8ec7582f54b42f3c7245b7be58ee903d
CynthiaOuma12673 <cynthia.ouma@student.moringaschool.com>
2022-03-13 09:10:34 +0300

    it will add the classes and the objects to be used in the application

---
+++ app/models.py
@@ -0,0 +1,74 @@
+from xml.etree.ElementTree import Comment
+from . import db, login_manager
+from werkzeug.security import generate_password_hash,check_password_hash
+from flask_login import UserMixin
+from datetime import datetime
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+class User(db.Model, UserMixin):
+    __tablename__='users'
+    id = db.Column(db.Integer,primary_key = True)
+    username = db.Column(db.String(255),index = True)
+    email = db.Column(db.String(255),unique = True,index = True)
+    password_hash = db.Column(db.String(255))
+    pass_secure = db.Column(db.String(255))
+    bio = db.Column(db.String(255))
+    profile_pic_path = db.Column(db.String())
+    pitches = db.relationship('Blog', backref='user', lazy='dynamic')
+    comment = db.relationship('Comment', backref='user', lazy='dynamic')
+
+    @property
+    def password(self):
+        raise AttributeError('Password attribute cannot be read')
+
+    @password.setter
+    def password(self, password):
+        self.pass_secure = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.pass_secure, password)
+
+    def __repr__(self):
+        return f'User{self.username}'
+
+class Blog(db.Model):
+    __tablename__ = 'blogs'
+    id = db.Column(db.Integer, primary_key = True)
+    title = db.Column(db.String(255))
+    post = db.Column(db.Text())
+    time = db.Column(db.DateTime, default = datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    comment = db.relationship("Comment",backref="pitch",lazy="dynamic")
+
+    def save_blog(self):
+        db.session.add(self)
+        db.session.commit()
+
+    @classmethod
+    def get_comments(cls,blog_id):
+        comments = Comment.query.filter_by(blog_id).all()
+
+        return comments
+
+    def __repr__(self):
+        return f'comment: {self.comment}'
+
+class Quote:
+    def __init__(self,author,quote):
+        self.author=author
+        self.quote=quote
+        
+class Subscribe(db.Model):
+    __tablename__= 'subscribe'
+    id=db.Column(db.Integer,primary_key=True)
+    email = db.Column(db.String(255),index=True)
+    
+    def save_subscribe(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def __repr__(self):
+        return f'subscribe:{self.email}'
