d30a8ef090b8066ffc6f881e7d1cd8b155fdc217
CynthiaOuma12673 <cynthia.ouma@student.moringaschool.com>
2022-03-13 10:52:23 +0300

    it will add the content of config file and start file

---
+++ start.sh
@@ -0,0 +1,5 @@
+export MAIL_USERNAME = 'appscynthia7@gmail.com'
+export MAIL_PASSWORD = 'Cyn12673#'
+export SECRET_KEY = '12673'
+export DATABASE_URL = 'postgresql+psycopg2://moringa:Cyn12673#@localhost/blogs'
+python3 manage.py server
