492cdf9d2563f2501e9f2169eb05ed97fd78f2a4
CynthiaOuma12673 <cynthia.ouma@student.moringaschool.com>
2022-03-15 18:58:40 +0300

    deploy

--- manage.py
+++ manage.py
@@ -2,6 +2,7 @@ from app import create_app,db
 from flask_script import Manager,Server
 from  flask_migrate import Migrate, MigrateCommand
 
+
 from app.models import User,Blog,Comment,Subscribe
 
 # Creating app instance
